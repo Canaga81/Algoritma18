37cd657caddaf5ce28edcb776fdd5d578b6beb0c
Canaga81 <Canagamemmedov@gmail.com>
2023-08-21 11:08:14 +0400

    Algoritma_28

---
+++ Algoritma_28/app.js
@@ -0,0 +1,21 @@
+let num = 2718;
+
+function AdditivePersistance(num) {
+    
+    let str = String(num);
+    let arr = str.split('');
+
+    let count = 0;
+
+    while(arr.length > 1) {
+        arr = String(arr.reduce(function(a, b) {
+            return Number(a) + Number(b)
+        })).split('');
+
+        count++;
+    }
+
+    console.log(count);
+}
+
+AdditivePersistance(num);
